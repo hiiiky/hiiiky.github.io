9c3ce52953d41573303d60aba55eb8a3d680356a
hiiiky <127434210+hiiiky@users.noreply.github.com>
2023-03-09 18:48:27 +0000

    tidy up

---
+++ src/components/TheAbout.vue
@@ -0,0 +1,40 @@
+<script setup>
+import AboutItem from '@/components/AboutItem.vue'
+import DocumentationIcon from '@/components/icons/IconDocumentation.vue'
+import SupportIcon from '@/components/icons/IconSupport.vue'
+</script>
+
+<template>
+  <AboutItem>
+    <template #icon>
+      <SupportIcon />
+    </template>
+    <template #heading>Born and raised</template>
+    Born and raised in Dublin Ireland
+  </AboutItem>
+
+  <AboutItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Education</template>
+    Studied Computer Science in Dublin Insitute of Technology (now TUD)
+  </AboutItem>
+
+  <AboutItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Work</template>
+    Work @ <a href="https://www.docosoft.com/" target="_blank">DOCOsoft</a> as a full stack
+    developer
+  </AboutItem>
+
+  <AboutItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Interests & Hobbies</template>
+    Football, Formula 1, DIY
+  </AboutItem>
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,11 @@
+<script setup>
+import TheAbout from '@/components/TheAbout.vue'
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <TheAbout />
+  </main>
 </template>
 
 <style>
